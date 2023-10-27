0e37a6fc1d565fcd7b396fa255f8acbac5979c10
Dairon Reijna <dairon.reijna@gmail.com>
2023-10-27 01:14:33 +0100

    resolved issues with npm run test
    Engineer.test.js
    TypeError: e.getGithub is not a function
    Solved: Amended line 18 - capital H - getGitHub.
    
    Manager.test.js
    ReferenceError: officeNumber is not defined
    Solved: added officeNumber to constructor on line 6 in Manager.js
    
    Employee.test.js
    school is not defined
    Solved: added school to constructor on line 6 in employee.js.

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -15,5 +15,5 @@ test("getRole() should return \"Engineer\"", () => {
 test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.getGithub()).toBe(testValue);
+  expect(e.getGitHub()).toBe(testValue);
 });
